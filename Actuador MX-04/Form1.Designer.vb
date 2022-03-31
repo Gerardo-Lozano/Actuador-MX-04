20171a245cff0add1716c170b92dc1794265f796
Gerardo Lozano <eselgera@gmail.com>
2022-03-31 09:53:13 -0600

    CREACION DE bOT 1.0

--- Actuador MX-04/Form1.Designer.vb
+++ Actuador MX-04/Form1.Designer.vb
@@ -27,13 +27,10 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.T_Status = New System.Windows.Forms.Timer(Me.components)
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.Label5 = New System.Windows.Forms.Label()
-        Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.Label8 = New System.Windows.Forms.Label()
-        Me.Label9 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
         'Label1
@@ -41,7 +38,7 @@ Partial Class Form1
         Me.Label1.AutoSize = True
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label1.ForeColor = System.Drawing.Color.LimeGreen
-        Me.Label1.Location = New System.Drawing.Point(107, 7)
+        Me.Label1.Location = New System.Drawing.Point(126, 65)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(49, 15)
         Me.Label1.TabIndex = 0
@@ -53,7 +50,7 @@ Partial Class Form1
         Me.Label2.AutoSize = True
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Label2.ForeColor = System.Drawing.Color.Red
-        Me.Label2.Location = New System.Drawing.Point(107, 7)
+        Me.Label2.Location = New System.Drawing.Point(126, 65)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(49, 15)
         Me.Label2.TabIndex = 1
@@ -65,108 +62,58 @@ Partial Class Form1
         Me.T_Status.Enabled = True
         Me.T_Status.Interval = 60000
         '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.ForeColor = System.Drawing.Color.Red
-        Me.Label3.Location = New System.Drawing.Point(107, 47)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(52, 15)
-        Me.Label3.TabIndex = 2
-        Me.Label3.Text = "Stoped"
-        Me.Label3.Visible = False
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.ForeColor = System.Drawing.Color.LimeGreen
-        Me.Label4.Location = New System.Drawing.Point(107, 47)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(61, 15)
-        Me.Label4.TabIndex = 3
-        Me.Label4.Text = "Running"
-        Me.Label4.Visible = False
-        '
-        'Label5
-        '
-        Me.Label5.AutoSize = True
-        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.ForeColor = System.Drawing.Color.LimeGreen
-        Me.Label5.Location = New System.Drawing.Point(107, 27)
-        Me.Label5.Name = "Label5"
-        Me.Label5.Size = New System.Drawing.Size(41, 15)
-        Me.Label5.TabIndex = 5
-        Me.Label5.Text = "Open"
-        Me.Label5.Visible = False
-        '
-        'Label6
-        '
-        Me.Label6.AutoSize = True
-        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.ForeColor = System.Drawing.Color.Red
-        Me.Label6.Location = New System.Drawing.Point(107, 27)
-        Me.Label6.Name = "Label6"
-        Me.Label6.Size = New System.Drawing.Size(51, 15)
-        Me.Label6.TabIndex = 4
-        Me.Label6.Text = "Closed"
-        Me.Label6.Visible = False
-        '
         'Label7
         '
         Me.Label7.AutoSize = True
         Me.Label7.ForeColor = System.Drawing.Color.White
-        Me.Label7.Location = New System.Drawing.Point(19, 8)
+        Me.Label7.Location = New System.Drawing.Point(38, 66)
         Me.Label7.Name = "Label7"
         Me.Label7.Size = New System.Drawing.Size(80, 13)
         Me.Label7.TabIndex = 6
         Me.Label7.Text = "Base de Datos:"
         '
-        'Label8
+        'PictureBox1
         '
-        Me.Label8.AutoSize = True
-        Me.Label8.ForeColor = System.Drawing.Color.White
-        Me.Label8.Location = New System.Drawing.Point(6, 28)
-        Me.Label8.Name = "Label8"
-        Me.Label8.Size = New System.Drawing.Size(93, 13)
-        Me.Label8.TabIndex = 7
-        Me.Label8.Text = "Ventana InduSoft:"
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(227, 8)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(137, 137)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 9
+        Me.PictureBox1.TabStop = False
         '
-        'Label9
+        'Label5
         '
-        Me.Label9.AutoSize = True
-        Me.Label9.ForeColor = System.Drawing.Color.White
-        Me.Label9.Location = New System.Drawing.Point(12, 48)
-        Me.Label9.Name = "Label9"
-        Me.Label9.Size = New System.Drawing.Size(87, 13)
-        Me.Label9.TabIndex = 8
-        Me.Label9.Text = "Status programa:"
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Italic), System.Drawing.FontStyle))
+        Me.Label5.ForeColor = System.Drawing.Color.LimeGreen
+        Me.Label5.Location = New System.Drawing.Point(12, 9)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(203, 30)
+        Me.Label5.TabIndex = 5
+        Me.Label5.Text = "A.L.T.E.A. BOT"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.Black
-        Me.ClientSize = New System.Drawing.Size(200, 74)
+        Me.ClientSize = New System.Drawing.Size(394, 201)
         Me.ControlBox = False
-        Me.Controls.Add(Me.Label9)
-        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.PictureBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
-        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.Opacity = 0.8R
         Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
-        Me.Text = "MX-04 Monitor"
+        Me.Text = "A.L.T.E.A. BOT Monitor 1.0"
         Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -175,11 +122,7 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents T_Status As Timer
-    Friend WithEvents Label3 As Label
-    Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
-    Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents Label8 As Label
-    Friend WithEvents Label9 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label5 As Label
 End Class
--- Actuador MX-04/Form1.vb
+++ Actuador MX-04/Form1.vb
@@ -1,5 +1,6 @@
 ﻿Imports System.Data.SqlClient
 Imports System.IO
+Imports System.Net.Mail
 
 Public Class Form1
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
@@ -12,65 +13,32 @@ Public Class Form1
 
     Private Sub T_Status_Tick(sender As Object, e As EventArgs) Handles T_Status.Tick
 
-
         Conexion_SQL()
         conexion.Close()
-        CheckIfisOpen()
-        CheckIfRunning()
+        Checkdelay()
+        'CheckIfRunning()
 
-
-        'Generar records en una tabla de cierta base de datos
+        'Registra el estado de salud del robot
 
         'Obtenemos la Hora y Fecha
         Dim Fecha_log As String = DateTime.Now.ToString("MM/dd/yyyy")
         Dim Hora_log As String = Date.Now.ToString("HH:mm:ss")
 
-        Dim EstadoPC As String
-
-        If Label1.Visible = True Then
-            EstadoPC = "Online"
-        Else
-            EstadoPC = "Offline"
-        End If
-
-        'Capturamos si la ventana del Indusoft esta abierta
-        Dim VentanaIduSoft As String
-
-        If Label5.Visible = True Then
-            VentanaIduSoft = "Open"
-        Else
-            VentanaIduSoft = "Closed"
-        End If
-
-        'Capturamos si la aplicación del Indusoft esta corriendo
-        Dim StatusIduSoft As String
-
-        If Label4.Visible = True Then
-            StatusIduSoft = "Running"
-        Else
-            StatusIduSoft = "Stoped"
-        End If
-
         'Generamos el Log con la cadena de SQL
         Try
             Dim cadena_save As String
 
-            cadena_save = "INSERT INTO [Fluxmonitor].[dbo].[Log_MX04] ([Fecha]
-      ,[Hora]
-      ,[Estado_PC]
-      ,[Ventana_InduSoft]
-      ,[Status_Aplicacion]) VALUES ('" & Fecha_log & "'
-      ,'" & Hora_log & "'
-      ,'" & EstadoPC & "'
-      ,'" & VentanaIduSoft & "'
-      ,'" & StatusIduSoft & "')"
+            cadena_save = "UPDATE [AlteaDB].[dbo].[altea_bot] " &
+            "SET [bot_date] = '" & Fecha_log & "',
+             [bot_hour]= '" & Hora_log & "'
+             WHERE control = 1 "
 
             Dim comando_save As SqlCommand
             comando_save = New SqlCommand(cadena_save, ConexionSQL.ConexionSQL)
             comando_save.ExecuteNonQuery()
 
         Catch ex As Exception
-            'MsgBox("PROBLEMA CON DISPARAR DATOS AL RESUMEN:" & ex.Message)
+            MsgBox("PROBLEMA CON DISPARAR DATOS AL RESUMEN:" & ex.Message)
         End Try
 
 
@@ -83,31 +51,111 @@ Public Class Form1
         If p.Count > 0 Then
             ' Process is running
             'MsgBox("Esta corriendo la aplicación")
-            Label3.Visible = False
-            Label4.Visible = True
+            'Label3.Visible = False
+            'Label4.Visible = True
         Else
             ' Process is not running
             'MsgBox("No esta corriendo la aplicación")
-            Label3.Visible = True
-            Label4.Visible = False
+            'Label3.Visible = True
+            'Label4.Visible = False
         End If
     End Sub
 
-    Dim s() As Process
 
-    Private Sub CheckIfisOpen()
-        s = Process.GetProcessesByName("Studio Manager")
-        If s.Count > 0 Then
-            'Process is running
-            'MsgBox("Esta abierta la aplicación")
-            Label6.Visible = False
-            Label5.Visible = True
-        Else
-            'Process is not running
-            'MsgBox("No esta abierta la aplicación")
-            Label6.Visible = True
-            Label5.Visible = False
-        End If
+    Private Sub Checkdelay()
+
+        Try
+            'Revisa si hay registros con mas de 24 Horas
+
+
+            Dim cadena_check As String
+            Dim listado As String
+
+            cadena_check = "SELECT top 1 batch, analyst, status, approve_date, approve_hour, DATEDIFF(HOUR , CONVERT(DATETIME, approve_date) + CONVERT(DATETIME, approve_hour), GETDATE()) as [hour_lapse] FROM [AlteaDB].[dbo].[batch_route] WHERE status = 0"
+
+            Dim comando_check As SqlCommand
+            comando_check = New SqlCommand(cadena_check, ConexionSQL.ConexionSQL)
+            comando_check.ExecuteNonQuery()
+
+            adaptador = New SqlDataAdapter(cadena_check, ConexionSQL.ConexionSQL)
+            ConexionSQL.registro = New DataSet
+            adaptador.Fill(registro, "Tabla3")
+            listado = registro.Tables("Tabla3").Rows.Count
+
+            Dim hora_lapso As Integer
+            Dim lote As String
+            hora_lapso = registro.Tables("Tabla3").Rows(0).Item("hour_lapse")
+            lote = registro.Tables("Tabla3").Rows(0).Item("batch")
+
+            If hora_lapso > 24 Then
+
+
+                'Enviar Correo 
+
+                Dim Smtp_Server As New SmtpClient
+                Dim e_mail As New MailMessage
+                Smtp_Server.UseDefaultCredentials = False
+                'Smtp_Server.Credentials = New Net.NetworkCredential("DoNotReply@ElementSolutionsInc.com", "")
+                'Smtp_Server.Port = 587
+                'Smtp_Server.EnableSsl = True
+                Smtp_Server.Host = "NDCA0SMTP01.pahprod.pahglobal.net"
+
+
+                Dim cadena_correos As String
+                Dim lista_correos As String
+
+                cadena_correos = "SELECT [config1] ,[correos] FROM [AlteaDB].[dbo].[Tabla3] WHERE config1 = 'tres'"
+
+                Dim comando_mails As SqlCommand
+                comando_mails = New SqlCommand(cadena_correos, ConexionSQL.ConexionSQL)
+                comando_mails.ExecuteNonQuery()
+
+                adaptador = New SqlDataAdapter(cadena_correos, ConexionSQL.ConexionSQL)
+                ConexionSQL.registro = New DataSet
+                adaptador.Fill(registro, "Tabla3")
+                lista_correos = registro.Tables("Tabla3").Rows.Count
+
+                Dim Destinatarios As String
+                Destinatarios = registro.Tables("Tabla3").Rows(0).Item("correos")
+
+
+                'Dim Destinatarios As String
+                'Destinatarios = registro.Tables("Tabla3").Rows(0).Item("correos")
+
+                'MsgBox(Destinatarios)
+
+                e_mail = New MailMessage
+                e_mail.From = New MailAddress("notification.Alphamty@macdermidalpha.com")
+                e_mail.To.Add(Destinatarios.ToString) '<--correos destino
+                e_mail.Subject = ("ALTEA: -Alerta de pasta con mas de 24 Horas")
+                e_mail.IsBodyHtml = False
+                e_mail.Body = ("ALERTA, La Pasta con Lote: " & lote & ", Excede las 24 horas desde su liberación sin haber sido ingresada al refrigerador")
+
+                Smtp_Server.Send(e_mail)
+                'MsgBox("Correo enviado")
+                'btnEnviar.Enabled = True
+                'txtAsunto.Text = ""
+                'txtMensaje.Text = ""
+
+
+                'ACTUALIZAR EL ESTATUS QUE YA SE ENVIO NOTIFICACION
+
+                Dim cadena_actualiza As String
+
+                cadena_actualiza = "UPDATE [AlteaDB].[dbo].[batch_route] " &
+                "SET [status] = 1 
+                WHERE [batch] = '" & lote & "'"
+
+                Dim comando_actualiza As SqlCommand
+                comando_actualiza = New SqlCommand(cadena_actualiza, ConexionSQL.ConexionSQL)
+                comando_actualiza.ExecuteNonQuery()
+
+            End If
+
+        Catch ex As Exception
+            'MsgBox("Problema con inspeccionar base de datos: " & ex.Message)
+        End Try
+
     End Sub
 
     Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
